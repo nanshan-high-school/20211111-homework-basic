ad05cc9948de3b1150b5bb7d030114d1e7dcb62c
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-11-18 00:16:04 +0800

    Create guessing.cpp

---
+++ 011325/guessing.cpp
@@ -0,0 +1,16 @@
+// https://replit.com/@andy8787main/guessing-Cai-Mi-Ma#main.cpp
+// https://github.com/nanshan-high-school/20211111-homework-basic
+#include <iostream>
+using namespace std;
+
+int main() {
+
+} 
+
+/*
+四種模式
+1.使用者猜測
+2.兩個使用者輪流猜測，看是誰猜到就是勝利者
+3.電腦猜測
+4.電腦跟使用者互猜
+*/
