84bc75332d154f3e028c1cea2f9d40d3b3f25092
張庭瑜 <tingyuchang960625@gmail.com>
2021-11-20 19:34:01 +0800

    855033作業

---
+++ 855033/855033作業.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int min = 1, ans, max = 100;
+    do {
+        cout << "請輸入" << min << "~" << max << "的數字" << "\n" ;
+        cin >> ans;
+        if (ans > 56) {
+            max = ans;
+        } else if (ans < 56){
+            min = ans;
+        }
+    } while (ans != 56);
+    cout << "你猜到了!";
+}
